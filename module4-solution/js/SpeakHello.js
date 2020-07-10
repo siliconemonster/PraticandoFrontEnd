bbf9cb5aa730facb1e9404377b8586ad07ba4b1d
aline <alinefrez@netscape.net>
2020-07-09 23:37:21 -0300

    Catched the name the function was sending

--- module4-solution/js/SpeakHello.js
+++ module4-solution/js/SpeakHello.js
@@ -20,8 +20,8 @@
   // Rewrite the 'speak' function such that it is attached
   // to the helloSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
-  helloSpeaker.speak = function () {
-    console.log(speakWord + " " + helloSpeaker.name)
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name)
   }
 
   // STEP 5: DONE
