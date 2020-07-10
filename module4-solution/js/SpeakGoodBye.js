d49618dd7e3b02f74bf709dbc8bcec4e7790fa84
aline <alinefrez@netscape.net>
2020-07-09 23:35:15 -0300

    Fixed typo

--- module4-solution/js/SpeakGoodBye.js
+++ module4-solution/js/SpeakGoodBye.js
@@ -22,7 +22,7 @@
   // the byeSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
   byeSpeaker.speak = function () {
-    console.log(speakWord + " " + bySpeaker.name);
+    console.log(speakWord + " " + byeSpeaker.name);
   }
 
   // STEP 9: DONE
