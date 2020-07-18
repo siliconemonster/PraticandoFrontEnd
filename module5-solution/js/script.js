557c6ba40567fb0ce7ecb66e746087eac54c8fc2
aline <alinefrez@netscape.net>
2020-07-18 00:12:16 -0300

    Fixed typos

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -99,11 +99,9 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
 
-      // TODO: STEP 2: Here, call chooseRandomCategory, passing
-      // it retrieved 'categories'
+      // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function
-      // returns vs what the chosenCategoryShortName
-      // variable's name implies it expects.
+      // returns vs what the chosenCategoryShortName variable's name implies it expects.
       var chosenCategoryShortName = "'"+chooseRandomCategory(categories).short_name+"'";
 
 
