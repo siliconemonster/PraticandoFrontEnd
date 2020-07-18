3620883ceb826b6e9840f7a086c5cf9ab98f11a0
aline <alinefrez@netscape.net>
2020-07-18 00:06:08 -0300

    Completed step 2

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -104,20 +104,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function
       // returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      $ajaxUtils.sendGetReques(
-        chooseRandomCategory,
-        function(chooseRandomCategory){
-
-
-          var chosenCategoryShortName = //....
-            buildMenuItemsViewHtml(categoryMenuItems,
-                                  menuItemsTitleHtml,
-                                  menuItemHtml);
-          insertHtml("#main-content", menuItemsViewHtml);
-        },
-        false);
-
-
+      var chosenCategoryShortName = "'"+chooseRandomCategory(categories).short_name+"'";
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -131,13 +118,12 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage; //= ....
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // ....
+
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
